2480348ab711dedb125f4760596f43b2a45a724d
SantosGarcia <santos@santos.com>
2015-11-13 18:00:16 +0100

    commit inicio de clase (Parte 1)

--- src/ch/makery/address/MainApp.java
+++ src/ch/makery/address/MainApp.java
@@ -50,7 +50,7 @@ public class MainApp extends Application {
         try {
             // Load person overview.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+            loader.setLocation(MainApp.class.getResource("view/PersonOverviewe.fxml"));
             AnchorPane personOverview = (AnchorPane) loader.load();
 
             // Set person overview into the center of root layout.
